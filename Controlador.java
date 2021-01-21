389b7998b9bb69c8b6b569d7200c777154ffa7d3
javi1201 <77763457+javi1201@users.noreply.github.com>
2021-01-20 23:27:09 -0600

    Se agrego la clase Modulo Radio
    Se implemento la clase modulo radio, la cual tiene los metodos y atributos que tiene que tener el radio para su correcta funcionalidad.

--- Controlador.java
+++ Controlador.java
@@ -70,4 +70,54 @@ public class Controlador{
     }
 }
 
+class ModuloRadio{
 
+    private boolean estadoRadio = false;
+    private boolean emisoraRadio = true; // True - AM ; False - FM
+
+    private Integer emisoraAMActual = 0;
+    private Integer emisoraFMActual = 0;
+
+    public Map< Integer , String > emisorasGuardadas = new HashMap<>();
+
+    public String[] emisorasAM = new String[]{"94.3","94.9","95.7","95.8","96.7","96.9","97.4","97.7","98.5","98.9"};
+    public String[] emisorasFM = new String[]{"80.5","80.6","82.2","82.7","83.6","83.7","85.1","85.6","89.3","89.5"};
+
+    boolean getestadoRadio(){
+        return estadoRadio;
+    }
+    void setestadoRadio(boolean x){
+        estadoRadio = x;
+    }
+    boolean getemisoraRadio(){
+        return emisoraRadio;
+    }
+    void setemisoraRadio(){
+        emisoraRadio = !emisoraRadio;
+    }
+    void setEmisoraRadioCambio(boolean x){
+        this.emisoraRadio = x;
+    }
+    Integer getEmisoraActual(boolean AM){
+        return AM ? this.emisoraAMActual : this.emisoraFMActual; 
+    }
+    void establecerPuntoAM(int pos){
+        this.emisoraAMActual = pos;
+    }
+    void establecerPuntoFM(int pos){
+        this.emisoraFMActual = pos;
+    }
+    void aumentarEmisoraActual(boolean AM){
+        if(AM){
+            if(emisoraAMActual == 9){
+                emisoraAMActual = 0;
+            } else { emisoraAMActual++; }
+        }
+        else{
+            if(emisoraFMActual == 9){
+                emisoraFMActual = 0;
+            } else { emisoraFMActual++; }
+        }
+    }
+
+}
