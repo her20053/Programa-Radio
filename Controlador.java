df3379389cee9b0293e8366f2dc5b9df15ccb7e9
Jose Hernandez Guerra <70614527+her20053@users.noreply.github.com>
2021-01-27 19:32:14 -0600

    Agregados comentarios al programa
    Se comento todas las funcionalidades dentro del programa tanto en atributos como metodos de las clases implementadas

--- Controlador.java
+++ Controlador.java
@@ -3,21 +3,45 @@ import java.util.Map;
 
 public class Controlador{
 
+    /**
+     * Metodo main que actua como controlador del programa
+     * @param args Argumento utilizado por Java
+     */
     public static void main(String[] args) {
-        
+        // Se crea la clase mv utilizada como sistema de prints para el programa:
         ModuloVista mv = new ModuloVista();
+        // Se crea la clase mr que actua como el objeto de radio para la ejecucion:
         ModuloRadio mr = new ModuloRadio();
+        // Se entra en un loop infinito para controlar el programa:
         while(true){
+            // Cada loop se hace un switch para saber que opcion desea realizar el usuario:
             switch (mv.seleccionarOpcionMI(mr.getestadoRadio(), mr.getemisoraRadio(), (mr.getemisoraRadio() ? mr.emisorasAM[mr.getEmisoraActual(true)] : mr.emisorasFM[mr.getEmisoraActual(false)]))) {
                 case 1:
+                    /**
+                     * Case 1
+                     * Encaragado de encender la radio
+                     * en caso de ya estar encendido enviara notificacion de su estado
+                     */
                     if(mr.getestadoRadio()){ mv.msgEstadoRadio(true); }
                     else{ mv.msgEstadoRadio(false); mr.setestadoRadio(true);;}
                     break;
                 case 2:
+                    /**
+                     * Case 2
+                     * Se revisa que la radio este encendida, si lo esta,
+                     * este enviara a la clase radio la informacion de que se
+                     * desea alterar entre AM o FM sin requerir parametro
+                     */
                     if(!mr.getestadoRadio()){ mv.msgERROREncenderRadio(); }
                     else{ mr.setemisoraRadio(); mv.msgEmisoraRadio(mr.getemisoraRadio()); }
                     break;
                 case 3:
+                    /**
+                     * Case 3
+                     * Se revisa que la radio este encendida, si lo esta,
+                     * este tomara la frecuencia en la que se encuentra el
+                     * radio actualmente y agregara una posicion a su emisora
+                     */
                     if(!mr.getestadoRadio()){ mv.msgERROREncenderRadio(); }
                     else{
                         mr.aumentarEmisoraActual(mr.getemisoraRadio());
@@ -25,6 +49,14 @@ public class Controlador{
                     }
                     break;
                 case 4:
+                    /**
+                     * Case 4
+                     * Se revisa que la radio este encendida, si lo esta,
+                     * revisara que aun hayan espacios disponibles (< 12)
+                     * verificado esto tomara la posicion que corresponde y
+                     * la emisora actual y seran guardadas en el HashMap de la
+                     * clase Radio "emisorasGuardadas"
+                     */
                     if(!mr.getestadoRadio()){ mv.msgERROREncenderRadio(); }
                     else{
                         if(mr.emisorasGuardadas.size() == 12){
@@ -36,6 +68,13 @@ public class Controlador{
                     }
                     break;
                 case 5:
+                    /**
+                     * Case 5
+                     * Este requiere que el usuario ingrese el boton que desea retraer del HashMap
+                     * una vez retraida la info. del boton siendo su posicion y emisora el programa
+                     * reconoce si es una frecuancia AM o FM y se encarga de alternar la frecuencia
+                     * en caso de no ser la misma y colocar la posicion de la emisora en la correspondiente.
+                     */
                     try {
                         int botonElecto = mv.ingresarBotonEmisoraGuardada();
                         String emisoraTEMP = mr.emisorasGuardadas.get(botonElecto);
@@ -61,6 +100,10 @@ public class Controlador{
                         System.out.println(" ! | Hubo un error al retraer tu emisora, porfavor intenta de nuevo..");
                     }
                     break;
+                /**
+                 * Caso 6
+                 * Apaga la radio
+                 */
                 case 6:
                     mr.setestadoRadio(false);
                     System.out.println(" ! | Radio apagado.");
@@ -70,6 +113,11 @@ public class Controlador{
     }
 }
 
+/**
+ * Interfaz con funcionalidades generales de una radio
+ * Es utilizada
+ * @author (Jose Hernandez, Javier Alvarez)
+ */
 interface RadioInterface {
 
     public int continuar();
@@ -77,45 +125,95 @@ interface RadioInterface {
     public void aumentarEmisoraActual(boolean AM);
 
 }
-
-
+/**
+ * Esta clase implementa la interfaz del funcionamiento de una radio
+ * Es utilizada
+ * @author (Jose Hernandez, Javier Alvarez)
+ */
 class ModuloRadio implements RadioInterface{
 
+    // Notifica si la radio esta encendida o apagada:
     private boolean estadoRadio = false;
+    // Atributo encargado de controlar la frecuencia de la emisora:
     private boolean emisoraRadio = true; // True - AM ; False - FM
-
+    // Posiciones en las que se encuentran las emisoras
+    // Estas van avanzando, al llegar al limite (11) se regresan a 0:
     private Integer emisoraAMActual = 0;
     private Integer emisoraFMActual = 0;
-
+    // Registro d emisorar guardadas, toman el numero en el que se guardan y la emisora como String:
     public Map< Integer , String > emisorasGuardadas = new HashMap<>();
-
+    // Listado de emisoras AM - FM:
     public String[] emisorasAM = new String[]{"94.3","94.9","95.7","95.8","96.7","96.9","97.4","97.7","98.5","98.9"};
     public String[] emisorasFM = new String[]{"80.5","80.6","82.2","82.7","83.6","83.7","85.1","85.6","89.3","89.5"};
 
+    /**
+     * Getter para si el radio esta apagado o encendido
+     * @return boolean
+     */
     boolean getestadoRadio(){
         return estadoRadio;
     }
+    /**
+     * Establece si el radio se enciende o apaga
+     * @param x
+     * @return void
+     */
     void setestadoRadio(boolean x){
         estadoRadio = x;
     }
+
+    /**
+     * Getter para saber si la emisora esta AM o FM
+     * @return boolean
+     */
     boolean getemisoraRadio(){
         return emisoraRadio;
     }
+
+    /**
+     * Establece true-false la emisora: AM o FM
+     */
     void setemisoraRadio(){
         emisoraRadio = !emisoraRadio;
     }
+    /**
+     * Establece la emisora segun se le pase parametro
+     * @param x
+     */
     void setEmisoraRadioCambio(boolean x){
         this.emisoraRadio = x;
     }
+
+    /**
+     * Utilizado para agarrar la emisora que esta siendo escuchada actualmente
+     * @param AM (Sirve para reconocer si se quiere retraer la emisora AM o FM)
+     * @return
+     */
     Integer getEmisoraActual(boolean AM){
         return AM ? this.emisoraAMActual : this.emisoraFMActual; 
     }
+    /**
+     * Una vez guardada una emisora, para elegirla se retrae la posicion del HashMap
+     * y se le asigna ese numero a la radio actual
+     * @param pos
+     */
     void establecerPuntoAM(int pos){
         this.emisoraAMActual = pos;
     }
+    /**
+     * Una vez guardada una emisora, para elegirla se retrae la posicion del HashMap
+     * y se le asigna ese numero a la radio actual
+     * @param pos
+     */
     void establecerPuntoFM(int pos){
         this.emisoraFMActual = pos;
     }
+
+    /**
+     * Aumenta la emisora que se escucha por una posicion, al llegar a la ultima posicion,
+     * este se volvera 0 de nuevo haciendo un loop entre la Array
+     * @param AM (Utilizado para ubicar que posicion de AM o FM se desea aumentar)
+     */
     public void aumentarEmisoraActual(boolean AM){
 
         if(AM){
@@ -130,13 +228,13 @@ class ModuloRadio implements RadioInterface{
         }
 
     }
-
+    // Metodo muerto utilizado por la interface
+    // Este metodo fue re escrito y se ubica en @aumentarEmisoraActual
     @Override
     public int continuar() {
         // TODO Auto-generated method stub
         return 0;
     }
-
     @Override
     public int retroceder() {
         // TODO Auto-generated method stub
--- ModuloVista.java
+++ ModuloVista.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
 
 public class ModuloVista{
-    
+    // Se inicializa la clase Scanner con el nombre sc (System.in) para retraer informacion del usuario:
     private Scanner sc = new Scanner(System.in);
 
+    /**
+     * Metodo encargado de controlar el menu principal del usuario
+     * @param rEncendido  Radio encendida o apagada, dependiendo de esto muestra iformacion distinta:
+     * @param aMfM        Para ubicar al usuario si su frencuancia es AM o FM
+     * @param emisora     Para ubicar al usuario en que emisora se encuentra actualmente
+     * @return opcion ( 1 - 6 )
+     */
     public int seleccionarOpcionMI(boolean rEncendido, boolean aMfM, String emisora){
         System.out.println("------------------------------------------\n");
         System.out.println("[•]| Bienvenido a tu radio personalizado! ");
@@ -22,31 +29,64 @@ public class ModuloVista{
         sc.nextLine();
         return opcion;
     }
+
+    /**
+     * Notifica si el usuario ya tiene el radio encendido u apagado.
+     * @param error (Reconoce si el radio es true o false siendo on/off)
+     */
     public void msgEstadoRadio(boolean error){
         System.out.println("\n------------------------------------------");
         if(error){ System.out.println(" ! | Tu radio ya esta encendido!");}
         else { System.out.println(" ! | Se ha encendido la radio!");}
     }
+    /**
+     * Notifica al usuario que la radio debe de estar encendida
+     */
     public void msgERROREncenderRadio(){
         System.out.println("\n------------------------------------------");
         System.out.println(" ! | Enciende tu radio para usar esta funcion!");
     }
+
+    /**
+     * Notifica al usuario el cambio de frecuencia de su emisora
+     * @param emisora siendo true-am false-fm
+     */
     public void msgEmisoraRadio(boolean emisora){
         System.out.println("\n------------------------------------------");
         System.out.println(" ! | Tu radio ha sido cambiado a " + (emisora ? "AM." : "FM."));
     }
+
+    /**
+     * Notifica al usuario la emisora actual registrada en el programa
+     * @param emisora String con la emisora
+     */
     public void msgCambioEmisora(String emisora){
         System.out.println("\n------------------------------------------");
         System.out.println(" ! | La emisora ha sido cambiada a: " + emisora);
     }
+
+    /**
+     * Al guardar la emisora, se le informa al usuario que emisora guardo y en que posicion del HashMap
+     * @param posicion numero en el que se ubica
+     * @param emisora  Nombre de la emisora (numero)
+     */
     public void msgEmisoraGuardada(int posicion, String emisora){
         System.out.println("\n------------------------------------------");
         System.out.println(" ! | La emisora [" + emisora + "] se ha guardado en el boton " + posicion + ".");
     }
+
+    /**
+     * Al no tener espacio, se notifica al usuario que se ha acabado su almacenamiento:
+     */
     public void msgERRORSinEspacio(){
         System.out.println("\n------------------------------------------");
         System.out.println(" ! | Ya no queda espacio para guardar mas emisoras!");
     }
+
+    /**
+     * Utilizada para ubicar que emisora necesita retraer el usuario:
+     * @return opcion (posicion electa)
+     */
     public int ingresarBotonEmisoraGuardada(){
         System.out.println("\n------------------------------------------");
         System.out.print(" ! | Porfavor ingresa el boton de la emisora: ");
@@ -54,6 +94,12 @@ public class ModuloVista{
         sc.nextLine();
         return opcion;
     }
+
+    /**
+     * Se notifica al usuario que la emisora cambio su frecuencia
+     * @param emisora La emisora actual String
+     * @param am      Siendo true-am false-fm
+     */
     public void cambioExitosoEmisora(String emisora, boolean am){
         System.out.println("\n------------------------------------------");
         System.out.println(" ! | Se ha cambiado exitosamente a: " + (am ? "AM" : "FM") + " [" + emisora + "] ");
